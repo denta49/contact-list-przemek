14025f6fe3feb0dc166412fa163dea3891c54bb2
Przemysław Wojenka <p.wojenka@361.sh>
2025-11-20 18:55:02 +0100

    import basic project files for startup

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import { Button } from "@/components/ui/button";
+import React from "react";
 
-export default function Home() {
+function App() {
+  // const [data, setData] = React.useState([]);
+  // const [selected, setSelected] = React.useState([]);
+
+  //  TODO fetch contacts using apiData function, handle loading and error states
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
-            Hello World!
-          </h1>
-        </div>
-        <Button>Test</Button>
-      </main>
+    <div className="App">
+      {/*<div className="selected">Selected contacts: {selected.length}</div>*/}
+      <div className="list">hello</div>
     </div>
   );
 }
+
+export default App;
